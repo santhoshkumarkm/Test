4c2711be0d26787c8081f30d2bd123bd27fc08a7
santhoshkumarkm <santhoshkumarkm@github.com>
2019-02-08 17:59:06 +0530

    feb8_3

--- Test/src/timer/JavaTimerScheduleAtFixedRateExample1.java
+++ Test/src/timer/JavaTimerScheduleAtFixedRateExample1.java
@@ -3,17 +3,22 @@ package timer;
 import java.util.*;
 
 public class JavaTimerScheduleAtFixedRateExample1 {
+	
+	
 	public static void main(String[] args) {
 // creating timer task, timer  
 		Timer t = new Timer();
 		TimerTask tt = new TimerTask() {
+			int index;
 			@Override
 			public void run() {
-				for (int i = 1; i <= 100; i++) {
+				System.out.println("Index : " + index);
+				for (int i = 1; i <= 10; i++) {
 					System.out.println("Task Timer on Fixed Rate" + i);
 				}
+				index++;
 			};
 		};
-		t.scheduleAtFixedRate(tt, 1000, 1000);
+		t.scheduleAtFixedRate(tt, 1000, 100);
 	}
 }
